d83ff8930d5bcfca1d71a3022a799f20ac46398d
amandine-sahl <amandine.sahl@gmail.com>
2018-06-06 11:52:49 +0200

    Correction inconsitance du datepicker

--- web_src/core/directives/form/datepick.directive.js
+++ web_src/core/directives/form/datepick.directive.js
@@ -15,19 +15,23 @@ angular.module('FormDirectives').directive('datepick', function(){
         templateUrl: 'js/templates/form/datepick.htm',
         controller: ['$scope', function($scope){
             $scope.opened = false;
+
             $scope.toggle = function($event){
                 $event.preventDefault();
                 $event.stopPropagation();
                 $scope.opened = !$scope.opened;
             };
 
-            if($scope.date){
-                try {
-                    $scope.date_display = new Date($scope.date);
-                  } catch (e) {
-                    
+            $scope.$watch('date', function(newval){
+                if ($scope.date){
+                    try {
+                        $scope.date_display = new Date($scope.date);
+                    } catch (e) {
+                        console.log(e); 
+                    }
                 }
-            }
+            });
+
             $scope.$watch('date_display', function(newval){
                 if (newval) {
                     $scope.date = newval.toISOString();
