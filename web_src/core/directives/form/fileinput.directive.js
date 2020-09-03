66e1d86bc064e0f9c7fc2284cfb533e0d79201da
amandine.sahl <amandine.sahl@gmail.com>
2020-09-03 14:36:52 +0200

    Coquille media

--- web_src/core/directives/form/fileinput.directive.js
+++ web_src/core/directives/form/fileinput.directive.js
@@ -17,19 +17,22 @@ angular.module('FormDirectives').directive('fileinput', function(){
             $scope.current_upload = null;
             $scope.progress = undefined;
 
-            var default_object = {
-                "id_table_location": $scope.options.id_table_location,
-                "uuid_attached_row": $scope.attacheduuid
-            };
+            $scope.init_object = function() {
+                $scope.default_object = {
+                    "id_table_location": $scope.options.id_table_location,
+                    "uuid_attached_row": $scope.attacheduuid
+                };
 
-
-            if($scope.refer == undefined || $scope.refer == null){
-                $scope.refer = [];
+                if($scope.refer == undefined || $scope.refer == null){
+                    $scope.refer = [];
+                }
             }
+
+            $scope.init_object();
 
             // Récupération des données types de média
             if ($scope.options.url_nomenclature_media_type == undefined) {
-                $scope.options.url_nomenclature_media_type = "nomenclatures/nomenclature/117"; 
+                $scope.options.url_nomenclature_media_type = "nomenclatures/nomenclature/117";
             }
             dataServ.get(
                 $scope.options.url_nomenclature_media_type,
@@ -39,19 +42,20 @@ angular.module('FormDirectives').directive('fileinput', function(){
             )
 
             $scope.items = angular.copy($scope.refer);
-            
+
             $scope.$watch(function(){return $scope.refer}, function(nv, ov){
                 if(nv !== ov && (!!ov && ov.length >0)  && (!!nv && nv.length >0)){
-                    $scope.refer = nv || [angular.copy(default_object)]; 
+                    $scope.refer = nv || [angular.copy($scope.default_object)];
                     if(!$scope.refer.length){
-                        $scope.refer.push(angular.copy(default_object));
+                        $scope.refer.push(angular.copy($scope.default_object));
                     }
                     $scope.items = angular.copy($scope.refer);
                 }
             });
-            
+
             $scope.add_item = function(){
-                $scope.refer.push(angular.copy(default_object));
+                $scope.init_object();
+                $scope.refer.push(angular.copy($scope.default_object));
             };
 
             $scope.remove_item = function(idx){
@@ -61,7 +65,7 @@ angular.module('FormDirectives').directive('fileinput', function(){
             $scope.delete_media = function(idx){
                 if ($scope.refer[idx].unique_id_media) {
                     dataServ.delete(
-                        'gn_commons/media/' + $scope.refer[idx].id_media, 
+                        'gn_commons/media/' + $scope.refer[idx].id_media,
                         function(resp){
                             $scope.remove_item(idx);
                         }
@@ -77,16 +81,16 @@ angular.module('FormDirectives').directive('fileinput', function(){
                     $scope.refer[idx] = angular.copy($scope.items[idx]);
                 }
                 else{
-                    $scope.refer[idx] = angular.copy(default_object);
+                    $scope.refer[idx] = angular.copy($scope.default_object);
                 }
             }
 
             $scope.is_valid = function(form, idx){
                 if(form.$valid){
                     if(
-                        ($scope.refer[idx].media_path && $scope.refer[idx].media_path.length) ^ 
-                        ($scope.refer[idx].media_url && $scope.refer[idx].media_url.length>0) ^ 
-                        ($scope.refer[idx].file_name && $scope.refer[idx].file_name.length>0)^ 
+                        ($scope.refer[idx].media_path && $scope.refer[idx].media_path.length) ^
+                        ($scope.refer[idx].media_url && $scope.refer[idx].media_url.length>0) ^
+                        ($scope.refer[idx].file_name && $scope.refer[idx].file_name.length>0)^
                         ($scope.refer[idx].id_nomenclature_media_type && $scope.refer[idx].id_nomenclature_media_type.length>0)
                     ){
                         var valid = !!($scope.refer[idx].title_fr && $scope.refer[idx].title_fr.length > 0);
@@ -113,7 +117,7 @@ angular.module('FormDirectives').directive('fileinput', function(){
             }
 
             $scope.action_upload = function(idx){
-                
+
                 url = 'gn_commons/media' + ($scope.refer[idx].id_media ? '/' + $scope.refer[idx].id_media : '')
 
                 data = angular.copy($scope.refer[idx]);
@@ -125,10 +129,10 @@ angular.module('FormDirectives').directive('fileinput', function(){
                 }
 
                 if ($scope.refer[idx].upload_file) {
-                    
+
                     data["file"] = $scope.refer[idx].upload_file;
                     data["isFile"] = true;
-                    
+
                     // Test type de fichier
                     if ($scope.options.accepted) {
                         $scope.options.accepted.map(function(item) { return item.toLowerCase();});
@@ -146,7 +150,7 @@ angular.module('FormDirectives').directive('fileinput', function(){
                         userMessages.errorMessage = "La taille du fichier doit être inférieure à " + (maxSize/1024) + " Ko";
                         return;
                     }
-    
+
                     Upload.upload({
                         url: RESOURCES.API_URL+url,
                         data: data
